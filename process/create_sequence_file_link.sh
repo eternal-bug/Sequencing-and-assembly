142289fae218b75bf5ade1f2a84937f3ec66167a
song tianqiang <31956395+eternal-bug@users.noreply.github.com>
2018-09-03 10:28:31 +0800

    Update create_sequence_file_link.sh

--- process/create_sequence_file_link.sh
+++ process/create_sequence_file_link.sh
@@ -1,9 +1,9 @@
 ROOTTMP=$(pwd)
 cd ${ROOTTMP}
-for name in $(ls ./sequence_data/*.gz')
+for name in $(ls ./sequence_data/*.gz)
 do
-  fq_basename=$(cat ${file} | perl -MFile::Basename -n -e '$new = basename($_);$new =~ s/[._]R\d+\.f(ast)*q\.gz//;print $new)
-  paired_end_num=$(cat ${file} | perl -MFile::Basename -n -e '$new = basename($_);$new =~ s/[._]R(\d+)\.f(ast)*q\.gz//;print $1)
+  fq_basename=$(cat ${file} | perl -MFile::Basename -n -e '$new = basename($_);$new =~ s/[._]R\d+\.f(ast)*q\.gz//;print $new')
+  paired_end_num=$(cat ${file} | perl -MFile::Basename -n -e '$new = basename($_);$new =~ s/[._]R(\d+)\.f(ast)*q\.gz//;print $1')
   if [ ! -d ${name} ];
   then
     # 新建文件夹
