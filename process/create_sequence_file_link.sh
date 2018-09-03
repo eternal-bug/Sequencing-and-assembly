a8b9cec1ca9df1fe5720a40252307700fd2b5c47
song tianqiang <31956395+eternal-bug@users.noreply.github.com>
2018-09-03 10:15:52 +0800

    Create create_sequence_file_link.sh

---
+++ process/create_sequence_file_link.sh
@@ -0,0 +1,26 @@
+ROOTTMP=$(pwd)
+cd ${ROOTTMP}
+for name in $(ls ./sequence_data/*.gz | perl -MFile::Basename -n -e '$new = basename($_);$new =~ s/[._]R\d+\.f(ast)*q\.gz//;print $new')
+do
+  if [ ! -d ${name} ];
+  then
+    # 新建文件夹
+    mkdir -p ${name}/1_genome
+    mkdir -p ${name}/2_illumina
+  else
+    # 建立链接
+    if [ -f ../../genome/genome.fa ];
+    then
+      cd ${name}/1_genome
+      ln -fs ../../genome/genome.fa genome.fa
+    fi
+    cd ${ROOTTMP}
+    if [ -f ../../sequence_data/${name}.R1.fastq.gz ];
+    then 
+      cd ${name}/2_illumina
+      ln -fs ../../sequence_data/${name}.R1.fastq.gz R1.fq.gz
+      ln -fs ../../sequence_data/${name}.R2.fastq.gz R2.fq.gz
+    fi
+    cd ${ROOTTMP}
+  fi
+done
