f9a05197350241fd78f578f7efafff4eb66cbc4c
song tianqiang <31956395+eternal-bug@users.noreply.github.com>
2018-09-03 10:17:22 +0800

    Update create_sequence_file_link.sh

--- process/create_sequence_file_link.sh
+++ process/create_sequence_file_link.sh
@@ -18,8 +18,8 @@ do
     if [ -f ../../sequence_data/${name}* ];
     then 
       cd ${name}/2_illumina
-      ln -fs ../../sequence_data/${name}.R1.fastq.gz R1.fq.gz
-      ln -fs ../../sequence_data/${name}.R2.fastq.gz R2.fq.gz
+      ln -fs ../../sequence_data/${name}*R1.f*.gz R1.fq.gz
+      ln -fs ../../sequence_data/${name}*R2.f*.gz R2.fq.gz
     fi
     cd ${ROOTTMP}
   fi
