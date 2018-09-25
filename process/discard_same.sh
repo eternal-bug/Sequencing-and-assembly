5c08aec44232939e67e9ead4228b77a424f21e25
song tianqiang <31956395+eternal-bug@users.noreply.github.com>
2018-09-25 18:38:08 +0800

    Update discard_same.sh

--- process/discard_same.sh
+++ process/discard_same.sh
@@ -25,6 +25,28 @@ perl -n -e '
   #   [title1,sequence1],
   #   [title2,sequence2],
   # ]
-  
-  # 先将序列按照从短到长进行排序
+  if(index($_,">")==0){
+    ($title = $_) =~ s/\r?\n//;
+    ($sequence = <>) =~ s/\r?\n//;
+    push @$list,[$title,$sequence];
+  }
+  # 将序列按照从短到长进行排序
+  END{
+    @list = sort {length($a->[1]) <=> length($b->[1])} @$list;
+    for my $host_num (0..scalar(@$list)-1){
+      my $flag = 1;
+      unless($host_num < scalar(@$list)-1){
+        MATCH:
+        for my $guest_num ($host_num+1..scalar(@$list)-1){
+          if(index($list->[$guest_num]->[1],$list->[$host_num]->[1]) != 0){
+            $flag = 0;
+            last MATCH;
+          }
+        }
+      }
+      if($flag == 1){
+        printf "%s\n%s\n",$list->[$host_num]->[0],$list->[$host_num]->[1];
+      }
+    }
+  }
 '
